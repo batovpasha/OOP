5237feac811a732595dbf557431ef9f617637d72
batovpasha <batovpasha@gmail.com>
2018-09-27 21:35:33 +0300

    Write template for second lab

---
+++ 2-lab/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+//   set<int> numbers;
+//   numbers.insert(123);
+//   numbers.insert(242);
+//   numbers.insert(456);
+//   numbers.insert(23);
+//   numbers.insert(76);
+//   numbers.insert(86);
+//   numbers.insert(4);
+//   set<int>::iterator it = numbers.begin();
+  
+//   while (it != numbers.end()) {
+//     cout << *it << " ";
+//     it++;
+//   }
+//   cout << endl;
+
+  return 0;
+}
