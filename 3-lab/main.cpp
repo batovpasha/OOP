b6350cc5f4ca6c5525b33a230a355755682f8a6b
batovpasha <batovpasha@gmail.com>
2018-10-18 11:37:04 +0300

    Some code optimization

--- 3-lab/main.cpp
+++ 3-lab/main.cpp
@@ -18,37 +18,37 @@ void testing()
     cout << "Enter number of number of subintervals: ";
     cin >> n;
 
+    Integration *integration;
 
     // calculating integral using trapezoidal rule
-    Integration *integrationByTrapezoidalRule = new TrapezoidalRule;
-    integrationByTrapezoidalRule->set_values(a, b, n);
+    integration = new TrapezoidalRule;
+    integration->set_values(a, b, n);
 
     cout << "Result of integration using trapezoidal rule: ";
-    integrationByTrapezoidalRule->print_result_of_calculating();
+    integration->print_result_of_calculating();
 
+    cout << "Delete TrapezoidalRule class object: ";
+    delete integration;
 
     // calculating integral using Left Riemann sum 
-    Integration *integrationByLeftRiemannSum = new LeftRiemannSum;
-    integrationByLeftRiemannSum->set_values(a, b, n);
+    integration = new LeftRiemannSum;
+    integration->set_values(a, b, n);
 
     cout << "Result of integration using Left Riemann sum: ";
-    integrationByLeftRiemannSum->print_result_of_calculating();    
+    integration->print_result_of_calculating();    
     
-    
+    cout << "Delete Left Riemann sum class object: ";
+    delete integration;
+
     // calculating integral using Right Riemann sum
-    Integration *integrationByRightRiemannSum = new RightRiemannSum;
-    integrationByRightRiemannSum->set_values(a, b, n);
+    integration = new RightRiemannSum;
+    integration->set_values(a, b, n);
 
     cout << "Result of integration using Right Riemann sum: ";
-    integrationByRightRiemannSum->print_result_of_calculating();
+    integration->print_result_of_calculating();
 
-
-    // Memory cleaning
-    cout << "Memory cleaning:" << endl;
-
-    delete integrationByTrapezoidalRule;
-    delete integrationByLeftRiemannSum;
-    delete integrationByRightRiemannSum;
+    cout << "Delete Right Riemann sum class object: ";
+    delete integration;
 }
 
 int main()
