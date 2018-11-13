e7a43901cd35845f79d21a2d0f804ac96d4cbcaa
batovpasha <batovpasha@gmail.com>
2018-11-13 09:11:53 +0200

    Wrote template for the fourth lab

--- 4-lab/currencyExchange.h
+++ 4-lab/currencyExchange.h
@@ -1,52 +1,123 @@
+#include <iostream>
+
+using namespace std;
+
 class Currency
 {   
     protected:
         float amount;
+        float exchangeRate;
 
     public:
         void  set_amount(float);
         float get_amount();
+        void  set_exchange_rate(float);
+        float get_exchange_rate();
 };
 
 class UAH: public Currency
 {
-    float exchangeRate;
-
     public:
+        UAH() {};
         UAH(float);
 };
 
 class USD: public Currency
 {
-    float exchangeRate;
-
     public:
+        USD() {};
         USD(float);
 };
 
 class EUR: public Currency
 {
-    float exchangeRate;
-
     public:
+        EUR() {};
         EUR(float);
 };
-
+ 
 template <class T>
 class Client
 {
-    T currency;
+    T amountToExchange;
 
     public:
         Client();
-        float get_amount();
+        float get_amount_to_exchange();
+        T get_currency();
 };
 
-template <class T>
+template <class FromCurrency, class ToCurrency>
 class Cashier
 {
-    T reserve;
-
     public:
-        Cashier();
+        bool check_reserve(FromCurrency, ToCurrency);
 };
+
+void Currency::set_amount(float _amount)
+{
+    amount = _amount;
+}
+
+float Currency::get_amount()
+{
+    return amount;
+}
+
+void Currency::set_exchange_rate(float _exchangeRate)
+{
+    exchangeRate = _exchangeRate;
+}
+
+float Currency::get_exchange_rate()
+{
+    return exchangeRate;
+}
+
+UAH::UAH(float exchangeRate)
+{
+    set_exchange_rate(exchangeRate);
+
+    set_amount(rand() % 3501 + 500); // set amount from 500 to 4000
+}
+
+USD::USD(float exchangeRate)
+{
+    set_exchange_rate(exchangeRate);
+
+    set_amount(rand() % 3501 + 500); // set amount from 500 to 4000
+}
+
+EUR::EUR(float exchangeRate)
+{
+    set_exchange_rate(exchangeRate);
+
+    set_amount(rand() % 3501 + 500); // set amount from 500 to 4000
+}
+
+template <class T>
+Client<T>::Client()
+{
+    amountToExchange.set_amount(rand() % 1501 + 500); // set amount from 500 to 2000
+}
+
+template <class T>
+float Client<T>::get_amount_to_exchange()
+{
+    return amountToExchange.get_amount();
+}
+
+template <class T>
+T Client<T>::get_currency()
+{
+    return amountToExchange;
+}
+
+template <class FromCurrency, class ToCurrency>
+bool Cashier<FromCurrency, ToCurrency>::check_reserve(FromCurrency clientAmount, ToCurrency reserveAmount)
+{  
+    if (reserveAmount.get_amount() >= clientAmount.get_amount() * reserveAmount.get_exchange_rate())
+        return true;
+
+    return false;
+}
--- 4-lab/main.cpp
+++ 4-lab/main.cpp
@@ -3,12 +3,33 @@
 
 using namespace std;
 
+void currencyExchange(string fromCurrency, string toCurrency, float rate)
+{   
+    Client<UAH> client;
+    
+    // generating currency reserve
+    UAH uah(rate);
+    USD usd(rate);
+    EUR eur(rate);
+
+    Cashier<UAH, USD> cashier;
+
+    cout << client.get_amount_to_exchange() << endl;
+    cout << usd.get_amount() << endl;
+    cout << usd.get_exchange_rate() << endl;
+
+    cout << cashier.check_reserve(client.get_currency(), usd) << endl;
+}
+
 int main(int argc, char **argv)
 {   
     string fromCurrency       (argv[1]); // 
-    string toCurrency         (argv[2]); // parsing data from command line
-    float  amount =       atof(argv[3]); //
-    float  exchangeRate = atof(argv[4]); // 
+    string toCurrency         (argv[2]); // parsing data from command line //
+    float  exchangeRate = atof(argv[3]); // 
+
+    srand(time(nullptr)); // for random numbers
+
+    currencyExchange(fromCurrency, toCurrency, exchangeRate);
 
     return 0;
 }
